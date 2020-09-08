be341e2e9cfd865d1d10d0c3c1172693f182f94f
Bytez <25850701+Bytezz@users.noreply.github.com>
2020-09-09 01:31:06 +0200

    0.1.1
    The print time it's shorter.

--- terminal_virtualface.py
+++ terminal_virtualface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os,sys,random
-version="0.1"
+import os,sys,random,thread
+version="0.1.1"
 def face(expr="normal",speak=False):
 	def clear():
 		os.system('cls' if os.name=='nt' else 'clear')
@@ -35,18 +35,26 @@ def face(expr="normal",speak=False):
 		sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8"%(y,x,text))
 		sys.stdout.flush()
 	####
-	eyebrowspos=[]
-	eyespos=[]
-	mouthpos=[]
+	runners=[]
+	#
+	eyebrowspos=None
+	eyespos=None
+	mouthpos=None
 	####
 	def eyebrows():
+		runners.append(1)
+		#
 		brow="#"*lenwidth(25)
 		# brow length, first[x,y], second[x,y]
 		pos=[len(brow),[lenwidth(12),lenheight(13)],[width()-lenwidth(12)-len(brow),lenheight(13)]]
 		printxy(pos[1][0],pos[1][1],brow)
 		printxy(pos[2][0],pos[2][1],brow)
+		#
+		runners.pop(0)
 		return pos
 	def eyes():
+		runners.append(1)
+		#
 		# left[top left corner[x,y], bottom right corner[x,y]], right[top left corner[x,y], bottom right corner[x,y]],
 		pos=[
 			[ # left
@@ -75,16 +83,28 @@ def face(expr="normal",speak=False):
 			printxy(eyebrowspos[2][0],eyebrowspos[2][1]+i,"#"+(" "*(percentage(75,eyebrowspos[0])-2))+"#")
 		printxy(eyebrowspos[1][0]+eyebrowspos[0]-percentage(75,eyebrowspos[0]),eyebrowspos[1][1]+i+1,"#"+("#"*(percentage(75,eyebrowspos[0])-2))+"#")
 		printxy(eyebrowspos[2][0],eyebrowspos[2][1]+i+1,"#"+("#"*(percentage(75,eyebrowspos[0])-2))+"#")
+		#
+		runners.pop(0)
 		return pos
 	def eyesdot():
+		runners.append(1)
+		#
 		eyewidth=eyespos[0][1][0]-eyespos[0][0][0]
 		eyeheight=eyespos[0][1][1]-eyespos[0][0][1]
 		printxy(eyespos[0][0][0]+percentage(50,eyewidth),eyespos[0][0][1]+percentage(50,eyeheight),"#")
 		printxy(eyespos[1][0][0]+percentage(50,eyewidth),eyespos[1][0][1]+percentage(50,eyeheight),"#")
+		#
+		runners.pop(0)
 	def nose():
+		runners.append(1)
+		#
 		n="#"*lenwidth(3)+" "*lenwidth(4)+"#"*lenwidth(3)
 		printxy(centerwidth(n),lenheight(45),n)
+		#
+		runners.pop(0)
 	def mouth():
+		runners.append(1)
+		#
 		m="#"*lenwidth(50)
 		# x, y
 		pos=[centerwidth(m),lenheight(70)]
@@ -95,14 +115,22 @@ def face(expr="normal",speak=False):
 		else:
 			m="#"*lenwidth(25)
 			printxy(centerwidth(m),lenheight(70)+1,m)
+		#
+		runners.pop(0)
 		return pos
 	####
 	clear()
+	#
+	thread.start_new_thread(nose,())
+	thread.start_new_thread(mouth,())
+	#
 	eyebrowspos=eyebrows()
 	eyespos=eyes()
 	eyesdot()
-	nose()
-	mouthpos=mouth()
+	#nose()
+	#mouthpos=mouth()
+	while runners!=[]:
+		pass
 
 if __name__=="__main__":
 	face()
